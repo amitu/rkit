fb3c59f86694a2aaca4ab90ff0ac4fa4cac5ac34
Amit Upadhyay <amitu@browserstack.com>
2015-03-19 00:47:29 +0530

    some docs

--- frames.go
+++ frames.go
@@ -9,10 +9,26 @@ import (
 )
 
 var (
+	/*
+		AnimationFrame is an "EventSource" that can be used to subscribe to
+		"EventFrame" events.
+
+		EventFrame is an event that is fired when the underlying graphic library
+		requests a new frame to be rendered.
+
+		If you want to do any kind of animation, instead of running a go routine
+		powered by a timer, power it by this event source. The timer you have
+		may fire more often than the underlying system updates the screen, so it
+		would be wastage of computational resources.
+	*/
 	AnimationFrame *EventSource
 	keepWatching   = false
 )
 
+/*
+	AnimationFrameEvent struct. This would be the concrete struct passed to
+	subscribers of AnimationFrame EventSource.
+*/
 type AnimationFrameEvent struct {
 	BaseEvent
 }
