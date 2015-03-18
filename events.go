3466dda20622f1a6c3254a7a2711c1b43a15d611
Amit Upadhyay <amitu@browserstack.com>
2015-03-18 23:31:47 +0530

    map access was not safe

--- events.go
+++ events.go
@@ -1,5 +1,7 @@
 package rkit
 
+import "sync"
+
 type EventType string
 
 type Event interface {
@@ -21,6 +23,7 @@ func (be BaseEvent) Type() EventType {
 
 type EventSource struct {
 	channels map[chan Event]struct{}
+	lock     sync.RWMutex
 }
 
 func MakeEventSource() *EventSource {
@@ -30,16 +33,31 @@ func MakeEventSource() *EventSource {
 }
 
 func (s *EventSource) Sub() chan Event {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
 	ch := make(chan Event)
 	s.channels[ch] = struct{}{}
 	return ch
 }
 
 func (s *EventSource) Unsub(ch chan Event) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
 	delete(s.channels, ch)
 }
 
 func (s *EventSource) Pub(ev Event) {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+
+	/*
+		TODO: how to decide if this is a safe pattern? can one lose some events
+		if they are busy processing stuff? i suppose if they really care, they
+		can spawn a goroutine dedicated to capturing all fired events, and que
+		them elsewhere and process them later.
+	*/
 	for ch, _ := range s.channels {
 		select {
 		case ch <- ev:
