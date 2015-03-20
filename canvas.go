2c37f9a9693b9246bfa33796bbb48fc09d31afad
Amit Upadhyay <amitu@browserstack.com>
2015-03-20 20:06:41 +0530

    golint happy

--- canvas.go
+++ canvas.go
@@ -6,29 +6,25 @@ import (
 )
 
 var (
-	/*
-		DesktopResize is an "EventSource" that can be used to subscribe to
-		"Desktop" resizing events.
-
-		This is not the same as window resize, as on one desktop one may have
-		multiple windows. We do not even have a concept of window so far, but
-		when do, that would be different than this.
-	*/
+	// DesktopResize is an "EventSource" that can be used to subscribe to
+	// "Desktop" resizing events.
+	//
+	// This is not the same as window resize, as on one desktop one may have
+	// multiple windows. We do not even have a concept of window so far, but
+	// when do, that would be different than this.
 	DesktopResize *EventSource
-	/*
-		Key is an EventSource, that can be used to subscribe to keyboard events.
 
-		This is a top level event. In most cases you do not need this, and want
-		a widget specific EventSource for keys.
-	*/
+	// Key is an EventSource, that can be used to subscribe to keyboard events.
+	//
+	// This is a top level event. In most cases you do not need this, and want a
+	// widget specific EventSource for keys.
 	Key *EventSource
-	/*
-		Mouse is an EventSource, that can be used to subscribe to mous/touch
-		events.
 
-		This is a top level event. In most cases you do not need this, and want
-		a widget specific EventSource for mouse/touch event.
-	*/
+	// Mouse is an EventSource, that can be used to subscribe to mous/touch
+	// events.
+	//
+	// This is a top level event. In most cases you do not need this, and want a
+	// widget specific EventSource for mouse/touch event.
 	Mouse    *EventSource
 	canvas   *js.Object
 	textarea *js.Object
@@ -36,30 +32,27 @@ var (
 	cursorMap map[string]cursor.Cursor
 )
 
-/*
-	DesktopResizeEvent struct. This would be the concrete struct passed to
-	subscribers of DesktopResize EventSource.
-*/
+// DesktopResizeEvent struct. This would be the concrete struct passed to
+// subscribers of DesktopResize EventSource.
 type DesktopResizeEvent struct {
 	BaseEvent
 }
 
-/*
-	Key and Mouse events have an attribute .Action of type Action. It can be
-	used to determine the action for the event.
-*/
+// Action is available on Key and Mouse events as .Action. It can be used to
+// determine the action for the event.
 type Action int
 
 var (
-	MOVE    = Action(0)
-	PRESS   = Action(1)
+	// MOVE is Mouse or Touch move Action
+	MOVE = Action(0)
+	// PRESS is Mouse, Touch, or Keyboard press Action
+	PRESS = Action(1)
+	// RELEASE is Mouse, Touch or Keyboard release Action
 	RELEASE = Action(2)
 )
 
-/*
-	KeyEvent is the struct that is sent over on Key event channel when a
-	keyboard event occurs.
-*/
+// KeyEvent is the struct that is sent over on Key event channel when a keyboard
+// event occurs.
 type KeyEvent struct {
 	BaseEvent
 	Char      rune
@@ -68,6 +61,8 @@ type KeyEvent struct {
 	Action    Action
 }
 
+// MouseEvent is the struct that is sent over on Mouse event channel when a
+// mouse or touch event occurs.
 type MouseEvent struct {
 	BaseEvent
 	X, Y   int
@@ -236,34 +231,25 @@ func initCanvas() {
 	body.Call("appendChild", textarea)
 }
 
-/*
-	Width() returns the width of the "desktop". Note this is not same as window.
-*/
+// Width returns the width of the "desktop". Note this is not same as window.
 func Width() int {
 	return js.Global.Get("window").Get("innerWidth").Int()
 }
 
-/*
-	Height() returns the height of the "desktop". Note this is not same as
-	window.
-*/
+// Height returns the height of the "desktop". Note this is not same as window.
 func Height() int {
 	return js.Global.Get("window").Get("innerHeight").Int()
 }
 
-/*
-	Title() returns the title of the "desktop". Note this is not same as window.
-	This may not be supported on all platforms.
-*/
+// Title returns the title of the "desktop". Note this is not same as window.
+// This may not be supported on all platforms.
 func Title() string {
 	return js.Global.Get("document").Get("title").String()
 }
 
-/*
-	SetTitle() changes the title "desktop". This may not be supported in all
-	platforms. In case of browswer, the title is shown in tab, but in iphone etc
-	it may not be shown anywhere.
-*/
+// SetTitle changes the title "desktop". This may not be supported in all
+// platforms. In case of browswer, the title is shown in tab, but in iphone etc
+// it may not be shown anywhere.
 func SetTitle(title string) {
 	js.Global.Get("document").Set("title", title)
 }
@@ -277,11 +263,13 @@ func cursor2css(c cursor.Cursor) string {
 	return ""
 }
 
+// GetCursor returns a cursor.Cursor object returning the current cursor.
 func GetCursor() cursor.Cursor {
 	tstyle := textarea.Get("style")
 	return cursorMap[tstyle.Get("cursor").String()]
 }
 
+// SetCursor takes a cursor.Cursor and makes it current.
 func SetCursor(c cursor.Cursor) {
 	tstyle := textarea.Get("style")
 	tstyle.Set("cursor", cursor2css(c))
--- events.go
+++ events.go
@@ -2,42 +2,71 @@ package rkit
 
 import "sync"
 
+// EventType is the type of event.
 type EventType string
 
+// Event is the interface every concrete event implements, and fired from any
+// EventSource.∑
 type Event interface {
+	// String returns the string representation of the event.
 	String() string
+	// Type ruturns the type of event
 	Type() EventType
 }
 
+// BaseEvent can be used as base of any event.
 type BaseEvent struct {
 	name string
 }
 
+// String returns string representation of the event.
 func (be BaseEvent) String() string {
 	return be.name
 }
 
+// Type returns the type of events.
 func (be BaseEvent) Type() EventType {
 	return EventType(be.name)
 }
 
+// EventSourceWatcher is an interface any EventSource can implement to be
+// notified when there is no one watching or when someone starts watching for
+// the first time.
 type EventSourceWatcher interface {
+	// Start is fired when the first subscriber for this event subscribes.
+	// EventSource can decide to start detecting events only after someone is
+	// actually listening to it
 	Start()
+	// Start is fired when the last subscriber has unsubscribed.
 	Stop()
 }
 
+// EventSource struct, the base struct, (TODO: make this an interface?), that
+// any eventsource represents.
 type EventSource struct {
+	// Channels stores a unique channel for each subscriber. The channel is
+	// buffered.
 	Channels map[chan Event]struct{}
-	Lock     sync.RWMutex
-	Watcher  EventSourceWatcher
+	// Lock used to make Channels safe. Can also be used by eventsource or
+	// EventSourceWatcher implementations for other reasons.
+	Lock sync.RWMutex
+	// Watcher can be nil if event source is not interested in Start()/Stop()
+	// events of EventSourceWatcher
+	Watcher EventSourceWatcher
 }
 
+// MakeEventSource can be used to create a new event source.
 func MakeEventSource() *EventSource {
 	return &EventSource{
 		Channels: make(map[chan Event]struct{}),
 	}
 }
 
+// Sub can be used for someone who is interested in an Event to listen for them.
+// Returns a Event channel, which is buffered.
+//
+// When the first guy subscribes, EventSourceWatcher.Start() is called if
+// EventSource.Watcher is not nil.
 func (s *EventSource) Sub() chan Event {
 	s.Lock.Lock()
 	defer s.Lock.Unlock()
@@ -51,6 +80,12 @@ func (s *EventSource) Sub() chan Event {
 	return ch
 }
 
+// Unsub can be used to unsubscribe from an EventSource, eg if you are no longer
+// interested in keyboard event. Must be called with the channel returned by
+// Sub() call.
+//
+// When the last guy unsubscribes, EventSourceWatcher.Stop() is called if
+// EventSource.Watcher is not nil.
 func (s *EventSource) Unsub(ch chan Event) {
 	s.Lock.Lock()
 	defer s.Lock.Unlock()
@@ -61,6 +96,7 @@ func (s *EventSource) Unsub(ch chan Event) {
 	}
 }
 
+// Pub is used to publish an event to all the subscribers of an eventsource.
 func (s *EventSource) Pub(ev Event) {
 	s.Lock.RLock()
 	defer s.Lock.RUnlock()
@@ -71,7 +107,7 @@ func (s *EventSource) Pub(ev Event) {
 		can spawn a goroutine dedicated to capturing all fired events, and que
 		them elsewhere and process them later.
 	*/
-	for ch, _ := range s.Channels {
+	for ch := range s.Channels {
 		select {
 		case ch <- ev:
 		default:
