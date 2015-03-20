4f8a118d30bf182d5940bfb02e91b84e86f5b5e2
Amit Upadhyay <amitu@browserstack.com>
2015-03-20 20:10:50 +0530

    lint/doc on cursor

--- cursor/cursor.go
+++ cursor/cursor.go
@@ -1,14 +1,16 @@
+// Package cursor represents the UI cursor by the pointing device.
 package cursor
 
-/*
-	The mouse cursor. Not every device will support cursors, but those that will
-	will have the following ones.
-
-	In future we may support arbitrary bitmaps as cursors, but its not really
-	priority, and they are so 2000.
-*/
+// Cursor represents the mouse cursor. Not every device will support cursors,
+// but those that will will have the following ones.
+//
+// In future we may support arbitrary bitmaps as cursors, but its not really
+// priority, and they are so 2000s.
 type Cursor int
 
+// These are the various cursors available. Not all may be supported. As of now
+// there is no way to programatically detect if one is supported or not, only
+// way to find out is by testing it across different drivers
 const (
 	Default Cursor = iota
 	Alias
